ec39246836204ac3507857b1807b91272e688ac8
nascimbr <nascimbr@d58b91e0-45a2-6f42-9374-62d56d83aa9a>
2011-08-10 01:34:56 +0000

    melhoria no usuario service.
    git-svn-id: https://localhost/svn/bruno_projects/trunk@37 d58b91e0-45a2-6f42-9374-62d56d83aa9a

--- cashbuilder/grails-app/services/com/cashbuilder/UsuarioService.groovy
+++ cashbuilder/grails-app/services/com/cashbuilder/UsuarioService.groovy
@@ -15,25 +15,22 @@ class UsuarioService {
 		
 		int ano = DateUtils.getCurrentYear()
 		Orcamento orcm = new Orcamento(ano: ano,user:user)
-		orcm.save(flush: true) 
+		orcm.save(flush: true)
 
-		
-		Scanner sc = new Scanner(file);
-
-		while (sc.hasNext()) {
-
-			String[] linhaBase = sc.nextLine().split(":")
+		file.eachLine{ linha ->
+			
+			String[] linhaBase = linha.split(":")
 			
 			if(linhaBase.length > 0 && !linhaBase[0].contains("#")){
 				
 				String categoria = linhaBase[0]
 				boolean receita = ("Receitas").equals(categoria)
-				
+
 				Categoria categoriaBean = new Categoria( nome:categoria, user:user, receita:receita ).save(flush: true)
 				
 				String[] subcategorias = linhaBase[1].split(";")
 				
-				for(String nome : subcategorias) { 
+				subcategorias.each { nome ->
 					new Subcategoria(nome: nome,categoria:categoriaBean).save(flush: true)
 				}
 			}
