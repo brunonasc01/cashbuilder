75d88fed86cba84d5257d8ab5d4b2f75eaa9dfa4
nascimbr <nascimbr@d58b91e0-45a2-6f42-9374-62d56d83aa9a>
2012-09-18 19:03:41 +0000

    Adicao do plugin de email Criacao da tabela para armazenar o token de recuperacao de senha
    git-svn-id: https://localhost/svn/bruno_projects/trunk@153 d58b91e0-45a2-6f42-9374-62d56d83aa9a

--- cashbuilder/grails-app/conf/Config.groovy
+++ cashbuilder/grails-app/conf/Config.groovy
@@ -94,3 +94,15 @@ log4j = {
 
 //correcao para o contexto na producao
 grails.app.context = "/"
+
+grails {
+	mail {
+	  host = "smtp.gmail.com"
+	  port = 465
+	  username = "contato@fazrico.com"
+	  password = "yourpassword"
+	  props = ["mail.smtp.auth":"true",
+			   "mail.smtp.socketFactory.port":"465",
+			   "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
+			   "mail.smtp.socketFactory.fallback":"false"]   }
+ }
---
+++ cashbuilder/grails-app/controllers/com/cashbuilder/RecoveryController.groovy
@@ -0,0 +1,22 @@
+package com.cashbuilder
+
+import org.joda.time.LocalDateTime;
+
+class RecoveryController {
+
+    def index() {
+	
+		def user = User.findByEmail("test@net.com")	
+		
+		if(user){
+			Random rand = new Random()			
+			LocalDateTime ldt = new LocalDateTime()
+			LocalDateTime expDtate = ldt.plusHours(2)
+			
+			String key = rand.nextInt(5)+"${user.id}"+expDtate.toString()
+			String token = Encoder.encode(key)
+			
+			new Recovery(user: user,token: token, expirationDate: expDtate).save()
+		}
+	}
+}
